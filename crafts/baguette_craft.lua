1f0149860c0d050173db3615f7759f6ae1d82cfb
Bapt-tech <137829833+Bapt-Tech@users.noreply.github.com>
2024-09-10 14:11:27 +0200

    Create baguette_craft.lua

---
+++ crafts/baguette_craft.lua
@@ -0,0 +1,7 @@
+minetest.register_craft({
+    output = "imstaff:baguette",
+    recipe = {,
+        {"mcl_farming:wheat_item", "mcl_farming:wheat_item"},
+        {"mcl_farming:wheat_item", "mcl_farming:wheat_item"}
+    }
+})
