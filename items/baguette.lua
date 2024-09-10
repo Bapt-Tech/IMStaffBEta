08935b61f0705ab008831e2a9fe7e748838d47c4
Bapt-tech <137829833+Bapt-Tech@users.noreply.github.com>
2024-09-10 14:13:46 +0200

    Update baguette.lua

--- items/baguette.lua
+++ items/baguette.lua
@@ -1,1 +1,9 @@
-
+minetest.register_craftitem("imstaff:baguette", {
+	description = S("BAguette"),
+	_doc_items_longdesc = S("This is a food item which can be eaten."),
+	inventory_image = "baguette.png",
+	groups = {food = 2, eatable = 6, compostability = 85},
+	_mcl_saturation = 9.0,
+	on_place = minetest.item_eat(5),
+	on_secondary_use = minetest.item_eat(5),
+})
