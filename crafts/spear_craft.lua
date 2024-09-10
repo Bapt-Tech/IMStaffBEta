0f9a2d08c3ad3b3889fb688bcef3c5df6d80b638
Bapt-tech <137829833+Bapt-Tech@users.noreply.github.com>
2024-09-10 14:12:02 +0200

    Create spear_craft.lua

---
+++ crafts/spear_craft.lua
@@ -0,0 +1,9 @@
+minetest.register_craft({
+    output = "imstaff:spear",
+    recipe = {
+        {"mcl_nether:netherite_ingot", "mcl_nether:netherite_ingot", "mcl_nether:netherite_ingot"},
+        {"mcl_mobitems:blaze_rod", "mcl_nether:netherite_ingot", "mcl_mobitems:blaze_rod"},
+        {"", "mcl_mobitems:blaze_rod", ""}
+    }
+})
+
