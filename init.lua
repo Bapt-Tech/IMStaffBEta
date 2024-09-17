61a3cb54259493b7277c0eadfb61370bb104ce9e
Bapt-tech <137829833+Bapt-Tech@users.noreply.github.com>
2024-09-17 21:50:53 +0200

    removing new import sys

--- init.lua
+++ init.lua
@@ -1,40 +1,50 @@
 --Privs
+--Admin Priv
+dofile(minetest.get_modpath("imstaff") .. "/privs/admin.lua")
+--/lookup command
+dofile(minetest.get_modpath("imstaff") .. "/privs/mod.lua")
 
-local privs = {
-	"admin",
-}
-
-for _, priv in ipairs(privs) do
-	dofile(imstaffprev.modpath .. "/privs/" .. privs .. ".lua")
-end
-
---Scripts
-
-local scripts = {
-	"spectate",
-	"announce",
-	"heal",
-	"openinvec",
-	"rename",
-	"quests",
-	"rickroll",
-	"textbox",
-	"lookup",
-	"adminnotify",
-}
-
-for _, script in ipairs(scripts) do
-	dofile(imstaffprev.modpath .. "/scripts/" .. script .. ".lua")
-end
+--Scripts for commands files
+--/spectate and /unspectate commands
+dofile(minetest.get_modpath("imstaff") .. "/scripts/spectate.lua")
+--/announce command
+dofile(minetest.get_modpath("imstaff") .. "/scripts/announce.lua")
+--/heal command
+dofile(minetest.get_modpath("imstaff") .. "/scripts/heal.lua")
+--/openinv and /openec commands
+dofile(minetest.get_modpath("imstaff") .. "/scripts/openinvec.lua")
+--/rename command
+dofile(minetest.get_modpath("imstaff") .. "/scripts/rename.lua")
+--/quests and /createquest commands
+dofile(minetest.get_modpath("imstaff") .. "/scripts/quests.lua")
+--/rickastley command
+dofile(minetest.get_modpath("imstaff") .. "/scripts/rickroll.lua")
+--/textbox command
+dofile(minetest.get_modpath("imstaff") .. "/scripts/textbox.lua")
+--/lookup command
+dofile(minetest.get_modpath("imstaff") .. "/scripts/lookup.lua")
+--/mute command
+dofile(minetest.get_modpath("imstaff") .. "/scripts/mute.lua")
+--/doas command dont work
+--dofile(minetest.get_modpath("imstaff") .. "/scripts/doas.lua")
 
 --Items
+--Spear
+dofile(minetest.get_modpath("imstaff") .. "/items/spear.lua")
+--Baguette
+dofile(minetest.get_modpath("imstaff") .. "/items/baguette.lua")
 
-local items = {
-	"spear",
-	"baguette",
-}
+--Crafts
+--Spear
+dofile(minetest.get_modpath("imstaff") .. "/crafts/spear_craft.lua")
+--Baguette
+dofile(minetest.get_modpath("imstaff") .. "/crafts/baguette_craft.lua")
 
-for _, item in ipairs(items) do
-	dofile(imstaffprev.modpath .. "/items/" .. items .. ".lua")
-end
+--Scripts
+--AdminNotify
+dofile(minetest.get_modpath("imstaff") .. "/scripts/adminnotify.lua")
+--ImperTips
+dofile(minetest.get_modpath("imstaff") .. "/scripts/impertips.lua")
 
+--Alias
+dofile(minetest.get_modpath("imstaff")) .. "/alias/alias.lua")
