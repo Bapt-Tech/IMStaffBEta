35a5b16c2c87ddb1420406f9f6b1d767688da0e0
Bapt-tech <137829833+Bapt-Tech@users.noreply.github.com>
2024-09-17 21:35:13 +0200

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,18 +1,3 @@
---Items
---Spear
-dofile(minetest.get_modpath("imstaffprev") .. "/items/spear.lua")
---Baguette
-dofile(minetest.get_modpath("imstaffprev") .. "/items/baguette.lua")
-
---Scripts
---AdminNotify
-dofile(minetest.get_modpath("imstaffprev") .. "/scripts/adminnotify.lua")
-
-imstaffprev = {}
-imstaffprev.modname = core.get_current_modname()
-imstaffprev.modpath = core.get_modpath(imstaffprev.modname)
-
-
 --Privs
 
 local privs = {
@@ -28,14 +13,14 @@ end
 local scripts = {
 	"spectate",
 	"announce",
-    "heal",
+	"heal",
 	"openinvec",
-    "rename",
-    "quests",
-    "rickroll",
-    "textbox",
-    "lookup",
-    "adminnotify",
+	"rename",
+	"quests",
+	"rickroll",
+	"textbox",
+	"lookup",
+	"adminnotify",
 }
 
 for _, script in ipairs(scripts) do
